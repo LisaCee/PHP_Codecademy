5d70a0be82727ae6bfb0f482de151ff90f164afe
LisaCee <lisaceedesign@gmail.com>
2020-01-26 11:25:24 -0800

    built in 3

--- built_in_functions/built_in_functions.php
+++ built_in_functions/built_in_functions.php
@@ -12,3 +12,12 @@ echo gettype($second);
 echo var_dump($first);
 echo var_dump($second);
 
+<?php
+namespace Codecademy;
+
+// Write your code below:
+echo(strrev(".pu ti peeK .taerg gniod er'uoY"));
+
+echo(strtolower("SOON, tHiS WILL Look NoRmAL."));
+
+echo(str_repeat("\nThere's no place like home.\n", 3));
