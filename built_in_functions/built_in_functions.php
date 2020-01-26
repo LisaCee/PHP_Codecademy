879f7b5b03a9e7f8f40e441669ebd726c59bab4f
LisaCee <lisaceedesign@gmail.com>
2020-01-26 11:41:04 -0800

    built in 7

--- built_in_functions/built_in_functions.php
+++ built_in_functions/built_in_functions.php
@@ -60,3 +60,16 @@ echo "\n";
 echo rand();
 echo "\n";
 echo rand(1, 52);
+
+<?php
+namespace Codecademy;
+
+$a = 29;
+$b = "You did it!";
+$c = STR_PAD_BOTH;
+$d = "*~*";
+
+// Write your code below:
+echo str_pad($b, $a ,$d, $c);
+
+
