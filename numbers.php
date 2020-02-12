4e91c78e2c2303fe17e8ac528cce2f0189e3bfe3
LisaCee <lisaceedesign@gmail.com>
2020-02-11 18:42:58 -0800

    numbers

---
+++ numbers.php
@@ -0,0 +1,49 @@
+<?php
+// Write your code below:
+// $number = 9;
+
+// echo $number;
+// echo "\n";
+
+// $float = 1.35;
+// echo $float;
+  
+//addition
+// echo 10+2;
+
+//number variables
+// $last_month = 1187.23;
+// $this_month = 1089.98;
+
+// echo $last_month - $this_month;
+  
+// multiplication and division
+// $num_languages = 4;
+// $months = 11;
+// $days = $months * 16;
+// $days_per_language = $days / $num_languages;
+
+// echo $days_per_language;
+  
+//exponents
+// echo 8 ** 2;
+
+// modulo
+// echo 82 % 6;
+
+// order of operations
+// echo 94 - 4.25 + 7 - (23.50 * 1.2) + (20/4);  
+
+// math shorthand
+$my_num = 5;
+$answer = $my_num;
+
+$answer += 2;
+$answer *= 2;
+$answer -= 2;
+$answer /= 2;
+
+$answer -= $my_num;
+
+echo $answer;
+  
