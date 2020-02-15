a4fe6f72da6ebe60a0cb1a801696daefbf23cc2c
LisaCee <lisaceedesign@gmail.com>
2020-02-15 11:32:05 -0800

    code block

--- loops.php
+++ loops.php
@@ -94,3 +94,21 @@ while ($i < 5):
   $i++;
   endwhile;
 ?>
+
+// codeblock
+<h1>Shoe Shop</h1>
+<?php
+$footwear = [
+  "sandals" => 4,
+  "sneakers" => 7,
+	"boots" => 3
+];
+?>
+<p>Our footwear:</p>
+<?php
+foreach ($footwear as $type => $brands):
+?>
+<p>We sell <?=$brands?> brands of <?=$type?></p>
+<?php
+endforeach;
+?>
