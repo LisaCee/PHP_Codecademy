51ab74456c206ba4662e1e59efa7fdaa91d1acda
LisaCee <lisaceedesign@gmail.com>
2020-02-16 10:34:47 -0800

    regex

---
+++ regex.php
@@ -0,0 +1,21 @@
+bark
+
+cat|dog
+
+[chr]at
+
+....\.
+
+[cde][a-u][a-k]
+
+\d\s\w\w\w\w\w\w
+
+(puppies|kitty cats) are my favorite!
+
+squea{3,5}k
+
+\d ducks? for adoption\?
+
+ho{2,}t
+
+^penguins\sare\scooler\sthan\sregular\sexpressions$
