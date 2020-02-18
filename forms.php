e04f12cddfd0d05858042db1a1a3be521d877ea4
LisaCee <lisaceedesign@gmail.com>
2020-02-17 16:23:03 -0800

    sanitizing data

--- forms.php
+++ forms.php
@@ -36,3 +36,49 @@ function checkWord($input, $letter){
     <h3>"b" is for: <?= $_POST["b-word"];?><h3>
     <h3>"c" is for: <?= $_POST["c-word"];?><h3>    
 <div>  
+
+<form method="post" action="">
+Enter some HTML:
+<br>
+<input type="text" name="html">
+<br>  
+<input type="submit" value="Submit">
+  
+</form>
+<div>
+  You entered:
+	<?= htmlspecialchars($_POST["html"]); ?> 
+</div>  
+
+<?php
+$validation_error = "";
+$user_answer = "";
+$submission_response = "";
+
+// Write your code here:
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  $user_answer = filter_var($_POST["answer"], FILTER_SANITIZE_NUMBER_INT);
+  
+  if ($user_answer === "-5"){
+    $submission_response = "Correct!";
+  } else {
+    $validation_error = "* Wrong answer. Try again.";
+  }
+}
+
+
+?>
+<h2>Time for a math quiz!</h2>
+<form method="post" action="">
+<h4>Question 1:</h4>  
+<p>What is 6 - 11?</p> 
+<input type="text" name="answer" id="answer" value="<?= $user_answer;?>">
+<br>
+<span class="error" id="error"><?= $validation_error;?></span> 
+<br> 
+<input type="submit" value="Submit Your Answer">
+</form>
+<div>
+  <p id="answer-display">Your answer was: <?= $user_answer;?></p>
+  <p id="submission-response"><?= $submission_response;?></p>
+</div>
