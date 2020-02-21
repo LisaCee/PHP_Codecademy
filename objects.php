fe787a420b3d9e7539f9516e24deaa1068d21ea6
LisaCee <lisaceedesign@gmail.com>
2020-02-20 18:39:20 -0800

    class inheritance

--- objects.php
+++ objects.php
@@ -49,3 +49,19 @@ $water->color = "black";
 
 echo $water->getInfo();
 
+<?php
+class Beverage {
+  public $temperature;
+  
+  function getInfo() {
+    return "This beverage is $this->temperature.";
+  }
+}
+
+class Milk extends Beverage {
+  function __construct() {
+    $this->temperature = "cold";
+  }
+}
+
+
