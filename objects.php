f340383f24dca1968bd6f27fe2051db63e2afb05
LisaCee <lisaceedesign@gmail.com>
2020-02-20 18:25:03 -0800

    class instances

--- objects.php
+++ objects.php
@@ -3,3 +3,14 @@
 class Beverage {
   public $color, $opacity, $temperature;
 };
+
+<?php
+class Beverage {
+  public $temperature, $color, $opacity;
+}
+
+$tea = new Beverage;
+
+$tea->temperature = "hot";
+
+echo $tea->temperature;
