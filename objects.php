80072596eaf783106a465b093731b7409bf5c98e
LisaCee <lisaceedesign@gmail.com>
2020-02-20 18:29:45 -0800

    methods

--- objects.php
+++ objects.php
@@ -14,3 +14,17 @@ $tea = new Beverage;
 $tea->temperature = "hot";
 
 echo $tea->temperature;
+
+<?php
+class Beverage {
+  public $temperature, $color, $opacity;
+  function getInfo() {
+    return "This beverage is $this->temperature and $this->color.";
+  }
+}
+
+$soda = new Beverage();
+$soda->color = "black";
+$soda->temperature = "cold";
+
+echo $soda->getInfo();
